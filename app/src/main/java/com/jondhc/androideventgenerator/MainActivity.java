6b74a4cc7d8d1956f100c3d41e3e3c659a349c0f
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2018-09-22 23:56:53 -0500

    Added classes and interface for requests.

--- app/src/main/java/com/jondhc/androideventgenerator/MainActivity.java
+++ app/src/main/java/com/jondhc/androideventgenerator/MainActivity.java
@@ -3,11 +3,17 @@ package com.jondhc.androideventgenerator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.amazonaws.mobileconnectors.lambdainvoker.*;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Regions;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
