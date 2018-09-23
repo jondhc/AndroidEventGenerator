14691b8f5d64a5d091f74940344c7c8e32e404a0
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2018-09-23 00:10:12 -0500

    Finished.

--- app/src/main/java/com/jondhc/androideventgenerator/MainActivity.java
+++ app/src/main/java/com/jondhc/androideventgenerator/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jondhc.androideventgenerator;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.amazonaws.mobileconnectors.lambdainvoker.*;
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
@@ -14,6 +17,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Create an instance of CognitoCachingCredentialsProvider
+        CognitoCachingCredentialsProvider cognitoProvider = new CognitoCachingCredentialsProvider(
+                this.getApplicationContext(), "us-east-1:9d253d63-aec6-441f-926f-88980f7d4b5b", Regions.US_EAST_1);
 
+// Create LambdaInvokerFactory, to be used to instantiate the Lambda proxy.
+        LambdaInvokerFactory factory = new LambdaInvokerFactory(this.getApplicationContext(),
+                Regions.US_EAST_1, cognitoProvider);
+
+// Create the Lambda proxy object with a default Json data binder.
+// You can provide your own data binder by implementing
+// LambdaDataBinder.
+        final MyInterface myInterface = factory.build(MyInterface.class);
+
+        RequestClass request = new RequestClass("Daniel", "Herrejón");
+// The Lambda function invocation results in a network call.
+// Make sure it is not called from the main thread.
+        new AsyncTask<RequestClass, Void, ResponseClass>() {
+            @Override
+            protected ResponseClass doInBackground(RequestClass... params) {
+                // invoke "echo" method. In case it fails, it will throw a
+                // LambdaFunctionException.
+                try {
+                    return myInterface.AndroidBackendLambdaFunction(params[0]);
+                } catch (LambdaFunctionException lfe) {
+                    Log.e("Tag", "Failed to invoke echo", lfe);
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(ResponseClass result) {
+                if (result == null) {
+                    return;
+                }
+
+                // Do a toast
+                Toast.makeText(MainActivity.this, result.getGreetings(), Toast.LENGTH_LONG).show();
+            }
+        }.execute(request);
     }
 }
